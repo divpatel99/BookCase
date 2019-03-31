8ca29164839fc0879c10710ea17b3b4536259ed5
div <tug43815@temple.edu>
2019-03-31 13:10:46 -0400

    connect to the internet

--- app/src/main/java/edu/temple/lab7/arrList.java
+++ app/src/main/java/edu/temple/lab7/arrList.java
@@ -1,49 +1,43 @@
 package edu.temple.lab7;
 
+import android.app.Application;
+import android.content.SharedPreferences;
+import android.util.Log;
+import android.widget.Toast;
+
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 
+import static android.content.Context.MODE_PRIVATE;
+
 public class arrList implements Serializable {
 
-    String bookName;
 
     public static boolean isLan = false;
     public static boolean isVer = false;
+    Application application;
 
 
+    public arrList(Application application) {
 
-
-    public arrList(String bookName) {
-        this.bookName = bookName;
-
+        this.application =application;
     }
 
-
-    public static ArrayList<arrList> displayBooks() {
-
-        ArrayList<arrList> books = new ArrayList<arrList>();
-
-        books.add(new arrList("Do Androids Dream of Electric Sheep?"));
-        books.add(new arrList("I Was Told There'd Be Cake"));
-        books.add(new arrList("To Kill a Mockingbird"));
-        books.add(new arrList("The Perks of Being a Wallflower"));
-        books.add(new arrList("The Man Without Qualities"));
-        books.add(new arrList("Cloudy With a Chance of Meatballs"));
-        books.add(new arrList("Where the Wild Things Are"));
-        books.add(new arrList("Me Talk Pretty One Day "));
-        books.add(new arrList("One Hundred Years of Solitude"));
-        books.add(new arrList("The Elephant Tree"));
-
+    public static  ArrayList<Book>  allBooksArraylist =new ArrayList<>();
+    public static ArrayList<String > displayBooksTitle(ArrayList<Book> arrayList) {
+        ArrayList<String> books = new ArrayList<>();
+        for (int i=0; i< arrayList.size();i++)
+        {
+            books.add(arrayList.get(i).title);
+        }
         return books;
     }
 
-    //public String getTitle() {
-    //	return bookName;
-//	}
 
-    @Override
-    public String toString() {
-        return bookName;
-    }
 
 }
