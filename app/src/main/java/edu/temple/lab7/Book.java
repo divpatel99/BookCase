c285da4a89f08cd1b30d04cff1e3f8e78ff3bb4d
div <tug43815@temple.edu>
2019-03-31 13:07:07 -0400

    Created to book class to get info from api

---
+++ app/src/main/java/edu/temple/lab7/Book.java
@@ -0,0 +1,21 @@
+package edu.temple.lab7;
+
+import java.io.Serializable;
+
+public class Book implements Serializable {
+
+    int id;
+    String title;
+    String author;
+    int published;
+    String coverURL;
+
+
+    public Book(int id, String title, String author, int published, String coverURL) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.published = published;
+        this.coverURL = coverURL;
+    }
+}
