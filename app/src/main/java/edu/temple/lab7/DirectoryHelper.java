69b33ed014eb213044433c31883d9accbfbc2e4e
div <tug43815@temple.edu>
2019-05-01 18:06:17 -0400

    bug with downloading file

--- app/src/main/java/edu/temple/lab7/DirectoryHelper.java
+++ app/src/main/java/edu/temple/lab7/DirectoryHelper.java
@@ -1,4 +1,46 @@
 package edu.temple.lab7;
 
-public class DirectoryHelper {
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
+
+public class DirectoryHelper extends ContextWrapper {
+
+    private static final String TAG = "DirectoryHelper";
+    public static final String ROOT_DIRECTORY_NAME = "DownloadManager";
+
+    private DirectoryHelper(Context context) {
+        super(context);
+        createFolderDirectories();
+    }
+
+    public static void createDirectory(Context context) {
+        new DirectoryHelper(context);
+    }
+
+    private boolean isExternalStorageAvailable() {
+        String extStorageState = Environment.getExternalStorageState();
+        return Environment.MEDIA_MOUNTED.equals(extStorageState);
+    }
+
+    private void createFolderDirectories() {
+        if (isExternalStorageAvailable())
+            createDirectory(ROOT_DIRECTORY_NAME);
+    }
+
+    private void createDirectory(String directoryName) {
+        if (!isDirectoryExists(directoryName)) {
+            File file = new File(Environment.getExternalStorageDirectory(), directoryName);
+            file.mkdir();
+            Log.d(TAG, "createDirectory: ");
+        }
+    }
+
+    private boolean isDirectoryExists(String directoryName) {
+        File file = new File(Environment.getExternalStorageDirectory() + "/" + directoryName);
+        return file.isDirectory() && file.exists();
+    }
 }
--- app/src/main/java/edu/temple/lab7/DownloadSongService.java
+++ app/src/main/java/edu/temple/lab7/DownloadSongService.java
@@ -1,4 +1,40 @@
 package edu.temple.lab7;
 
-public class DownloadSongService {
+import android.app.DownloadManager;
+import android.app.IntentService;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.webkit.MimeTypeMap;
+import android.webkit.URLUtil;
+
+import edu.temple.lab7.dbHelper.DatabaseHelper;
+
+public class DownloadSongService extends IntentService {
+    private static final String DOWNLOAD_PATH = "edu.temple.lab7.androiddownloadmanager_DownloadSongService_Download_path";
+    private static final String DESTINATION_PATH = "edu.temple.lab7.androiddownloadmanager_DownloadSongService_Destination_path";
+    private static final String SAVE_DB= "SAVE_DB";
+    private static DatabaseHelper db;
+    private String fileImageName;
+    public DownloadSongService() {
+        super("DownloadSongService");
+    }
+    public static Intent getDownloadService(final @NonNull Context callingClassContext, final @NonNull String downloadPath, final @NonNull String destinationPath,String saveDB) {
+        db = new DatabaseHelper(callingClassContext);
+        return new Intent(callingClassContext, DownloadSongService.class)
+                .putExtra(DOWNLOAD_PATH, downloadPath)
+                .putExtra(DESTINATION_PATH, destinationPath)
+                .putExtra(SAVE_DB,saveDB);
+
+    }
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        String downloadPath = intent.getStringExtra(DOWNLOAD_PATH);
+        String destinationPath = intent.getStringExtra(DESTINATION_PATH);
+        String checkSAVEDB= intent.getStringExtra(SAVE_DB);
+        startDownload(downloadPath, destinationPath,checkSAVEDB);
+    }
+   
 }
